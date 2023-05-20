b4074da68612e3587bf8d30021d4774f78bdb446
ronlynes <takuma2460.0131@gmail.com>
2023-05-20 21:11:57 +0200

    feat: add introduction and education

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div>
       <div className="flex mx-auto w-[800px] mt-12">
-        <div className="mr-12 ">
+        <div className="mr-12 mt-4">
           <img
             src="/icon.png"
             className="
@@ -12,12 +12,13 @@ export default function Page() {
               object-cover
               w-full
               ring
-              ring-gray-300"
+              ring-gray-300
+              block"
             alt="Takuma Nishimura"
           />
         </div>
         <div className="">
-          <h2 className="font-semibold text-center text-3xl mb-3">
+          <h2 className="font-semibold text-3xl mb-3 ">
             Takuma Nishimura
           </h2>
           <p className="text-gray-800">
@@ -34,10 +35,29 @@ export default function Page() {
           </p>
         </div>
       </div>
-      <div className="mt-12">
-        <h2 className="font-semibold text-4xl">
+      <div className="mx-auto w-[900px] mt-12">
+        <h2 className="font-semibold text-3xl">
           Education
         </h2>
+        <div className="text-right">
+          <h3 className="text-2xl mb-2">
+            Friedrich-Alexander-Universität Erlangen-Nürnberg
+          </h3>
+          <p>Master's degree</p>
+          <p>Artifitial intelligence</p>
+          <p>Computer Vision, Computer Gprahics, Machine Learning, DeepLearning</p>
+          <p>2021.10.01 〜 2024.03.31</p>
+        </div>
+        <div className="text-right mt-8">
+          <h3 className="text-2xl mb-2">
+            The University of Electro-Communications
+          </h3>
+          <p>Bachelor's Degree</p>
+          <p>Security Informatics Program, School of Informatics and Engineering</p>
+          <p>The University of Electro-Communications</p>
+          <p>Mathematics, Computer, Cyber Security</p>
+          <p>2016.04.01 〜 2021.03.31</p>
+        </div>
       </div>
     </div>
   );
