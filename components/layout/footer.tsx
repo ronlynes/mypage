335c43cc19830ccf934e1c9b7908b9948e117df8
ronlynes <takuma2460.0131@gmail.com>
2023-05-20 21:10:12 +0200

    feat: add footer

---
+++ components/layout/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="absolute mx-auto left-1/2 transform -translate-x-1/2 bottom-5">
+      <small className="text-center">
+        &copy; 2023 Takuma Nishimura All Rights Reserved.
+      </small>
+    </footer>
+  );
+}
